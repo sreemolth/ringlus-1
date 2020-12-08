9ec4c03c491e390b27dcdf19c5123f2c1ef221fa
Anju-P <anjuprasannan321@gmail.com>
2020-12-08 19:17:19 +0530

    workorder changes

--- ringlus/ringlus/doctype/delivery_note/delivery_note.py
+++ ringlus/ringlus/doctype/delivery_note/delivery_note.py
@@ -1,17 +1,19 @@
 import frappe
-
+from frappe import _
 from frappe.model.naming import set_name_by_naming_series
 
-
 def delivery_note_on_save(doc,Handler=""):
-    item_list = frappe.db.sql("""select do.item_code,do.against_sales_order,do.product_serial_no
-    from `tabDelivery Note Item` do
-    """,as_dict=1)
-    for d in item_list:
-        order_and_dispatch_list=frappe.db.sql("""select oi.production_plan,oi.product_serial_no,oi.sales_invoice_status
-        from `tabProduction Plan Status` oi where sales_order=%s and  product_serial_no=%s
-        """,(d.against_sales_order,d.product_serial_no),as_dict=1)
-        if order_and_dispatch_list:
-            frappe.msgprint(order_and_dispatch_list.sales_invoice_status)
-            frappe.db.sql("""update `tabProduction Plan Status` set delivery_status = %s where product_serial_no =%s""",((doc.status),d.product_serial_no))
+    for i in doc.items:
+        item_list = frappe.db.sql("""select do.item_code,do.against_sales_order,do.product_serial_no
+        from `tabDelivery Note Item` do where against_sales_order=%s and product_serial_no=%s
+        """,(i.against_sales_order,i.product_serial_no),as_dict=1)
+        for d in item_list:
+            order_and_dispatch_list=frappe.db.sql("""select oi.production_plan,oi.product_serial_no,oi.sales_invoice_status
+            from `tabProduction Plan Status` oi where sales_order=%s and  product_serial_no=%s
+            """,(d.against_sales_order,d.product_serial_no),as_dict=1)
+            for x in order_and_dispatch_list:
+                if(x.sales_invoice_status=="Open"):
+                    frappe.throw(_("Sales Invoice is required"))
+                else:
+                    frappe.db.sql("""update `tabProduction Plan Status` set delivery_status = %s where product_serial_no =%s""",((doc.status),d.product_serial_no))
     return item_list
--- ringlus/ringlus/doctype/production_plan/production_plan.py
+++ ringlus/ringlus/doctype/production_plan/production_plan.py
@@ -1,20 +1,8 @@
 import frappe
-import frappe, json
-from frappe import msgprint, _
-from six import string_types, iteritems
-
-from frappe.model.document import Document
-from frappe.utils import cstr, flt, cint, nowdate, add_days, comma_and, now_datetime, ceil
-from frappe.utils.csvutils import build_csv_response
-from erpnext.manufacturing.doctype.bom.bom import validate_bom_no, get_children
-from erpnext.manufacturing.doctype.work_order.work_order import get_item_details
-from erpnext.setup.doctype.item_group.item_group import get_item_group_defaults
-from frappe.model.document import Document
-from frappe.utils import cstr, flt, cint, nowdate, add_days, comma_and, now_datetime, ceil
+from datetime import date
+from frappe.utils import nowdate
 from frappe.model.naming import set_name_by_naming_series
-from erpnext.manufacturing.doctype.bom.bom import validate_bom_no, get_children
-from erpnext.manufacturing.doctype.work_order.work_order import get_item_details
-from erpnext.setup.doctype.item_group.item_group import get_item_group_defaults
+from frappe.model.naming import make_autoname
 
 def production_plan_on_save(doc,Handler=""):
     
@@ -22,71 +10,47 @@ def production_plan_on_save(doc,Handler=""):
     from `tabProduction Plan Status` oi where sales_order=%s and production_plan=%s
     """,(doc.sales_order,doc.production_plan_name),as_dict=1)
     if order_and_dispatch_list:
-        
         frappe.db.sql("""update `tabProduction Plan Status` set production_plan_status = %s where production_plan =%s""",((doc.status),doc.production_plan_name))
+    item_list = frappe.db.sql("""select p.production_plan_name,pi.item_code from `tabProduction Plan` p inner join `tabProduction Plan Item` pi where p.name=pi.parent and p.name =%s""", (doc.name), as_dict=1)
+    item_list2 = frappe.db.sql("""select ps.sales_order from `tabProduction Plan` p inner join `tabProduction Plan Sales Order` ps where p.name=ps.parent and p.name=%s""", (doc.name), as_dict=1)
+    if doc.status == 'Completed':
+        today = nowdate()
+        x = frappe.utils.get_datetime(today).strftime('%d')
+        y=frappe.utils.get_datetime(today).strftime('%m')
+        z=frappe.utils.get_datetime(today).strftime('%Y')
+        for s in item_list:
+            for r in item_list2:
+                item2 = frappe.new_doc('Product Serial No')
+                item2.flags.ignore_permissions  = True
+                item2.serial_no = make_autoname('RNG'+x+y+z+'-.###')
+                item2.production_plan_name = s.production_plan_name
+                item2.item_code = s.item_code
+                item2.sales_order = r.sales_order
+                item2.update({
+                    'serial_no':item2.serial_no,
+                    'production_plan_name':item2.production_plan_name,
+                    'item_code':item2.item_code,
+                    'sales_order':item2.sales_order
+                    }).insert()
+
+        serial_no_list = frappe.db.sql("""select serial_no from `tabProduct Serial No` where production_plan_name=%s""",(doc.production_plan_name),as_dict=1)
+        #plan_list = frappe.db.sql("""select production_plan_name from `tabProduction Plan` where production_plan_name=%s""",(doc.name), as_dict=1)
+        for x in serial_no_list:
+            frappe.db.sql("""update `tabProduction Plan` set product_serial_no = %s where production_plan_name =%s""",((x.serial_no), doc.production_plan_name))
+            #frappe.db.sql("""update `tabProduction Plan Status` set product_serial_no = %s where production_plan =%s""",((x.serial_no), doc.production_plan)) 
+                #frappe.db.sql("""update `Production Plan Status` set product_serial_no = %s where production_plan = %s""",((y.production_plan_name), doc.production_plan))
+        plan_list = frappe.db.sql("""select p.product_serial_no from `tabProduction Plan` p  where p.production_plan_name =%s""", (doc.production_plan_name), as_dict=1)
+        for y in plan_list:
+            frappe.db.sql("""update `tabProduction Plan Status` set product_serial_no = %s where production_plan = %s""",((y.product_serial_no), doc.production_plan_name))
+        #for m in item_list:
+        #    for n in serial_no_list:
+        #        doc.product_serial_no=n.serial_no
+                #count=count+1
+                #item = frappe.new_doc('Production Plan')
+                #item.product_serial_no=n.serial_no
+                #item.flags.ignore_permissions  = True
+                #item.update({
+                #    'product_serial_no':item.product_serial_no
+                #}).insert()
     return order_and_dispatch_list
-  
-def make(doc,Handler=""):
-        '''Create Material Requests grouped by Sales Order and Material Request Type'''
-        material_request_list = []
-        material_request_map = {}
-        msgprint(_(doc.name))
-        for item in doc.mr_items:
-            item_doc = frappe.get_cached_doc('Item', item.item_code)
-            material_request_type = item.material_request_type or item_doc.default_material_request_type
 
-            # key for Sales Order:Material Request Type:Customer
-            key = '{}:{}:{}'.format(item.sales_order, material_request_type, item_doc.customer or '')
-            schedule_date = add_days(nowdate(), cint(item_doc.lead_time_days))
-
-            if not key in material_request_map:
-                # make a new MR for the combination
-                material_request_map[key] = frappe.new_doc("Material Request")
-                material_request = material_request_map[key]
-                material_request.update({
-                    "transaction_date": nowdate(),
-                    "title":doc.production_plan_name+'_'+material_request_type,
-                    #"production_plan_no":doc.name,
-                    "production_plan_name":doc.production_plan_name,
-                    "status": "Draft",
-                    "company": doc.company,
-                    "requested_by": frappe.session.user,
-                    'material_request_type': material_request_type,
-                    'customer': item_doc.customer or ''
-                })
-                material_request_list.append(material_request)
-            else:
-                material_request = material_request_map[key]
-
-            # add item
-            material_request.append("items", {
-                "item_code": item.item_code,
-                "qty": item.quantity,
-                "schedule_date": schedule_date,
-                "warehouse": item.warehouse,
-                "sales_order": item.sales_order,
-                'production_plan': item.name,
-                #'production_plan_name':item.production_plan_name,
-                'material_request_plan_item': item.name,
-                "project": frappe.db.get_value("Sales Order", item.sales_order, "project") \
-                    if item.sales_order else None
-            })
-
-        for material_request in material_request_list:
-            # submit
-            material_request.flags.ignore_permissions = 1
-            material_request.run_method("set_missing_values")
-
-            if doc.get('submit_material_request'):
-                material_request.submit()
-            else:
-                material_request.save()
-
-        frappe.flags.mute_messages = False
-
-        if material_request_list:
-            material_request_list = ["""<a href="#Form/Material Request/{0}">{1}</a>""".format(m.name, m.name) \
-                for m in material_request_list]
-            msgprint(_("{0} created").format(comma_and(material_request_list)))
-        else :
-            msgprint(_("No material request created"))
