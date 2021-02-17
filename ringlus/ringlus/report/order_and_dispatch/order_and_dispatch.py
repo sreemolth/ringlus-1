e5783673f55284482a2c6a8c2ee9eabe0a56038a
anulakshmiv <anulakshmiv123@gmail.com>
2021-02-17 10:45:49 +0530

    order nd dispatch change

--- ringlus/ringlus/report/order_and_dispatch/order_and_dispatch.py
+++ ringlus/ringlus/report/order_and_dispatch/order_and_dispatch.py
@@ -8,7 +8,6 @@ from frappe.utils import getdate
 
 def execute(filters=None):
 	columns, data = [], []
-	todo_list = frappe.get_list('ToDo', fields=["title", "posting_date", "status"],
-		filters={'status': 'Open'})
-    columns = [_("ID")+":Link/Order And Dispatch:90", _("Posting Date")+"::60", _("Status")+ ":Date"]
+	todo_list = frappe.get_list('ToDo', fields=["title", "posting_date", "status"],filters={'status': 'Open'})
+	columns = [_("ID")+":Link/Order And Dispatch:90", _("Posting Date")+"::60", _("Status")+ ":Date"]
 	return columns, data
