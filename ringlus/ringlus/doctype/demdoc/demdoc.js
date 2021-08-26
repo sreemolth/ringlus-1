a43cf3c50b06b28e35da4b0b657cd3d1e08f7616
saranyamomscode123 <saranya.th@momscode.in>
2021-08-26 14:53:42 +0530

    new doctype demdoc

---
+++ ringlus/ringlus/doctype/demdoc/demdoc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Momscode Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Demdoc', {
+	// refresh: function(frm) {
+
+	// }
+});
